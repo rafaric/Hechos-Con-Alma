8b0ebf9906dbe78ee671febc453186123b5478eb
Rafaric <44025206+rafaric@users.noreply.github.com>
2022-08-06 13:23:55 -0300

    Update develop.py

--- g3blog/settings/develop.py
+++ g3blog/settings/develop.py
@@ -1,2 +1,13 @@
 from .base import *
+import psycopg2
 postgres://cyfbcdcazardlq:9f8d55b52e7545d856a320906b5b7c33fd72ab6fcaf616b3fd94d2ec0d1efaba@ec2-3-213-228-206.compute-1.amazonaws.com:5432/dfnc0o9p1lcmpn
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hechosconalma',
+        'USER': 'cyfbcdcazardlq',
+        'PASSWORD': '9f8d55b52e7545d856a320906b5b7c33fd72ab6fcaf616b3fd94d2ec0d1efaba',
+        'HOST': 'ec2-3-213-228-206.compute-1.amazonaws.com',
+        'PORT': '',
+    }
+}
